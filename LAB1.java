f896e22f1b60c7822d4a250c74806051827faaf6
SaadatTariq <69859128+SaadatTariq@users.noreply.github.com>
2022-08-27 10:40:30 +0600

    First Commit

---
+++ LAB1.java
@@ -0,0 +1,34 @@
+package Lab.Lab1;
+
+import java.util.*;
+
+public class LAB1 {
+    
+	public static void main(String[] args) {
+
+				int numOfStudents = 10;
+				
+				char gender = 'M';
+				
+				String anthem = ("Amar Shonar Bangla, \nAmi tomay bhalo bashi");
+				
+				
+				//add "f" to determine number as float
+				float cgpa = 3.56f;
+				double gpa = 3.96;
+				
+				System.out.println("Hello World!");
+				System.out.println("Your term cgpa is: " + cgpa);
+				
+				Scanner scanner = new Scanner(System.in);
+				int userInput = scanner.nextInt();
+				System.out.println("Your Input + 10: " + (userInput+10));
+				System.out.println(anthem);
+				
+				
+				
+				
+			}
+	}
+
+
